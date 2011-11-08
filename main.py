fdcefc32652f9a22f1a51c687738690f468ee77c
James King <revjimmyking@gmail.com>
2011-11-07 21:13:56 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,283 @@
+#!/usr/bin/env python
+import webapp2
+import jinja2
+import os
+import models
+from webapp2_extras import auth
+from webapp2_extras import sessions
+from google.appengine.ext import db
+import logging
+from datetime import datetime,timedelta	
+
+jenv = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/views/"))
+
+def require_login(handler):
+	"""
+		Decorator for checking if there's a user associated with the current session.
+		Will also fail if there's no session present.
+	"""
+	def check_login(self, *args, **kwargs):
+		auth = self.auth
+		if not auth.get_user_by_session():
+			# If handler has no login_url specified invoke a 403 error
+			try:
+				self.redirect(('/login?redirect=%s'%self.request._environ['PATH_INFO']), abort=True)
+			except (AttributeError, KeyError), e:
+				self.abort(403)
+		else:
+			return handler(self, *args, **kwargs)
+
+	return check_login
+
+# Base handler that sets up session
+class BaseHandler(webapp2.RequestHandler): 
+	def dispatch(self):
+		# Get a session store for this request.
+		self.session_store = sessions.get_store(request=self.request)
+		
+		try:
+			# Dispatch the request. 
+			webapp2.RequestHandler.dispatch(self)
+		finally:
+			# Save all sessions. 
+			self.session_store.save_sessions(self.response)
+	
+	@webapp2.cached_property
+	def session(self):
+		# Returns a session using the default cookie key. 
+		return self.session_store.get_session()
+
+	@webapp2.cached_property
+	def auth(self):
+		return auth.get_auth()
+
+	@webapp2.cached_property
+	def session_store(self):
+		return sessions.get_store(request=self.request)
+		
+	def respond(self, template,*args, **kwargs):
+		user_vars = self.auth.get_user_by_session()
+		
+		if user_vars:
+			kwargs['login_name'] = user_vars['first_name']
+
+		kwargs['_url'] = self.request.environ['PATH_INFO']
+			
+		template = jenv.get_template(template)
+		self.response.out.write(template.render(*args,**kwargs))
+		
+# Handle index
+class MainPage(BaseHandler):
+	def get(self):
+		self.respond('index.html')
+
+# Handle login
+class Login(BaseHandler):
+	def get(self):
+		"""
+			Returns a simple HTML form for login
+		"""
+		self.respond('login.html', redirect = self.request.GET.get('redirect'))
+
+	def post(self):
+		"""
+			username: Get the username from POST dict
+			password: Get the password from POST dict
+		"""
+		email = self.request.POST.get('email')
+		password = self.request.POST.get('password')
+		redirect = self.request.POST.get('redirect')
+		
+		# Try to login user with password
+		# Raises InvalidAuthIdError if user is not found
+		# Raises InvalidPasswordError if provided password doesn't match with specified user
+		try:
+			user = self.auth.get_user_by_password(email, password)
+			self.redirect(redirect);
+		except (auth.InvalidAuthIdError, auth.InvalidPasswordError), e:
+			# Returns error message to self.response.write in the BaseHandler.dispatcher
+			# Currently no message is attached to the exceptions
+			self.redirect('/login?error=%s'%'BadLogin');
+
+class CreateUser(BaseHandler):
+	def get(self):
+		self.respond("new_user.html")
+	def post(self):
+		"""
+		Get the info from POST dict
+		"""
+		email = self.request.POST.get('email')
+		first_name = self.request.POST.get('first_name')
+		last_name = self.request.POST.get('last_name')
+		password = self.request.POST.get('password')
+
+		try:
+			user = models.User(email = email, 
+							first_name=first_name,
+							last_name=last_name,
+							password=password)
+			
+			models.User.create_user(user)
+			self.redirect('/login')
+		except(Exception), e:
+			self.get()
+
+class EditUser(BaseHandler):
+	@require_login
+	def get(self):
+		user_vars = self.auth.get_user_by_session()
+		user = models.User.get(user_vars['user_id'])
+		
+		user_form = { 'email': user.email, 'first_name': user.first_name, 'last_name': user.last_name, 'key': user.key() }
+
+		self.respond("user.html", user_form)
+
+	@require_login
+	def post(self):
+		"""
+		Get the info from POST dict
+		"""
+		email = self.request.POST.get('email')
+		first_name = self.request.POST.get('first_name')
+		last_name = self.request.POST.get('last_name')
+		key = self.request.POST.get('key')
+		
+		user = models.User.get(key)
+		
+		if not user:
+			self.get()
+			return
+		
+		try:
+			user.email = email
+			user.first_name = first_name
+			user.last_name = last_name
+			user.put()
+			self.redirect('/')
+		except(Exception), e:
+			self.response.out.write(e)
+
+class ChangePassword(BaseHandler):
+	@require_login
+	def get(self):
+		self.respond('password.html')
+
+	@require_login
+	def post(self):
+		current_password = self.request.POST.get('current_password')
+		new_password = self.request.POST.get('new_password')
+		confirm_password = self.request.POST.get('confirm_password')
+		user_vars = self.auth.get_user_by_session()
+		
+		user = models.User.get(user_vars['user_id'])
+		
+		if not user:
+			self.get()
+			return
+		
+		if user.password != models.User.make_password_hash(current_password):
+			self.response.out.write("Old password doesn't match")
+			return
+		elif new_password != confirm_password:
+			self.response.out.write("New password does not match confirmation.")
+			return
+		
+		try:
+			user.password = models.User.make_password_hash(new_password)
+			user.put()
+			self.redirect('/')
+		except(Exception), e:
+			self.response.out.write(e)
+			
+class Logout(BaseHandler):
+	"""
+		Destroy user session and redirect to login
+	"""
+	def get(self):
+		self.auth.unset_session()
+		# User is logged out, let's try redirecting to login page
+		try:
+			self.redirect('/')
+		except (AttributeError, KeyError), e:
+			return "User is logged out"
+
+class Activity(BaseHandler):
+	@require_login
+	def get(self):
+		self.respond('activity.html')
+
+	@require_login
+	def post(self):
+		user_vars = self.auth.get_user_by_session()
+		created = self.request.POST.get('created')
+		count = self.request.POST.get('count')
+		
+		try:
+			activity = models.Activity(key_name="%s:%s"%(user_vars['user_id'],created),
+									count=int(count), 
+									created=datetime.strptime(created,'%m/%d/%Y'), 
+									type="walking", 
+									parent=user_vars['user_id'])
+			activity.put()
+			self.redirect('/dashboard')
+		except Exception, e:
+			self.response.out.write(e)
+
+class UserDashBoard(BaseHandler):
+	@require_login
+	def get(self):
+		user_vars = self.auth.get_user_by_session()
+		activities = models.Activity.gql('WHERE ANCESTOR IS :1 order by created', user_vars['user_id'])
+		now = datetime.now()
+		now = datetime(now.year,now.month,now.day)
+		start = now - timedelta(days=28)
+
+		cal = {}
+		while start <= now:
+			cal[start] = None;
+			start = start + timedelta(days=1)
+		
+		for activity in activities:
+			cal[activity.created] = activity
+		
+		self.respond('user_dashboard.html', cal=sorted(cal.iteritems()))
+
+# List of compitition winners
+class LeaderBoard(BaseHandler):
+	def get(self):
+		self.response.out.write("Leader Board")
+
+class ReqLook(BaseHandler):
+	def get(self):
+		for key, value in self.request.environ.iteritems():
+			self.response.out.write("%s=%s<br>"%(key,value))
+
+config = {}
+config['webapp2_extras.sessions'] = {'secret_key': 'my-super-secret-key', 
+						'cookie_name': 'wtwc'}
+						
+config['webapp2_extras.auth'] = {'user_model': 'models.User', 
+								 'cookie_name': 'wtwc',
+								 'session_backend':'memcache',
+								 'user_attributes':['email','first_name','is_admin',]}
+
+logging.getLogger().setLevel(logging.DEBUG)
+app = webapp2.WSGIApplication([('/', MainPage),
+							('/login', Login),
+							('/user', EditUser), 
+							('/leaders', LeaderBoard),
+							('/register', CreateUser),
+							('/password', ChangePassword),
+							('/logout', Logout),
+							('/activity', Activity),
+							('/dashboard',UserDashBoard),
+							('/req', ReqLook)],
+							config = config,
+                            debug=True)
+
+def main():
+    app.run()
+
+if __name__ == '__main__':
+    main()
---
+++ models.py
@@ -0,0 +1,126 @@
+from google.appengine.ext import db
+from webapp2_extras import auth
+from hashlib import sha1
+import webapp2
+import hmac
+import logging
+import time
+
+class User(db.Model):
+	"""Universal user model. Can be used with App Engine's default users API,
+	   own auth or third party authentication methods (OpenId, OAuth etc).
+	"""
+	password_salt = "Hb987*&d7dhS"
+	
+	# Creation date.
+	created = db.DateTimeProperty(auto_now_add=True)
+    
+	# Modification date.
+	updated = db.DateTimeProperty(auto_now=True)
+    
+	# User defined unique name, also used as key_name.
+	first_name = db.StringProperty(required=True)
+    
+    # User defined unique name, also used as key_name.
+	last_name = db.StringProperty(required=True)
+    
+	# Password, only set for own authentication.
+	password = db.StringProperty(required=False)
+    
+	# User email
+	email = db.StringProperty(required=False)
+    
+	# Admin flag.
+	is_admin = db.BooleanProperty(default=False)
+
+	def get_id(self):
+		"""Returns this user's unique ID, which can be an integer or string."""
+		return self.key()
+
+	@classmethod
+	def make_password_hash(self, password):
+		return hmac.new(self.password_salt, 
+						password, 
+						sha1).hexdigest()
+
+	@classmethod
+	def create_user(cls, user):
+		usercount = User.gql("WHERE email = :1", user.email).count()
+		
+		if usercount > 0:
+			raise Exception('Email already registered')
+		
+		user.password = cls.make_password_hash(user.password)
+		
+		user.put()
+
+	@classmethod
+	def get_by_auth_token(cls, user_id, token):
+		"""
+		Returns a user object based on a user ID and token.
+		:param user_id: The user_id of the requesting user.
+        :param token: The token string to be verified.
+        :returns: A tuple ``(User, timestamp)``, with a user object and
+		the token timestamp, or ``(None, None)`` if both were not found.
+		"""
+		if cls.make_password_hash(str(user_id)) != token:
+			return (None, None)
+		
+		user = cls.get(user_id)
+		
+		if not user:
+			return (None, None)
+
+		return (user, int(time.time()))
+
+	@classmethod
+	def get_by_auth_password(cls, auth_id, password):
+		"""
+		Returns a user object, validating password.
+		:param auth_id: Authentication id.
+		:param password: Password to be checked.
+		:returns: A user object, if found and password matches.
+		:raises: ``auth.InvalidAuthIdError`` or ``auth.InvalidPasswordError``.
+		"""
+		password = cls.make_password_hash(password)
+		
+		logging.debug('password: %s'%password)
+		
+		user = cls.gql("WHERE email = :1 and password = :2", auth_id, password).get()
+        
+		if not user:
+			raise auth.InvalidAuthIdError()
+        	
+		return user
+
+	@classmethod
+	def create_auth_token(cls, user_id):
+		"""
+		Creates a new authorization token for a given user ID.
+		:param user_id: User unique ID.
+		:returns: A string with the authorization token.
+		"""
+		return cls.make_password_hash(str(user_id))
+
+	@classmethod
+	def delete_auth_token(cls, user_id, token):
+		"""
+		Deletes a given authorization token.
+		:param user_id: User unique ID.
+		:param token: A string with the authorization token.
+		"""
+
+class Activity(db.Model):
+	created = db.DateTimeProperty(auto_now_add=True)
+	count = db.IntegerProperty(required=True)
+	type = db.StringProperty(required=True)
+	
+class Compitition(db.Model):
+	start = db.DateTimeProperty(required=True)
+	end = db.DateTimeProperty(required=True)
+	name = db.StringProperty(required=True)
+	
+class Comment(db.Model):
+	created = db.DateTimeProperty(auto_now_add=True)
+	text = db.StringProperty(required=True)
+	parent_comment = db.SelfReferenceProperty()
